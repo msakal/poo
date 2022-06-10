d7ce7026654ae3929dfffe75cb831ebfaf0dd8eb
msakal <m_sakalauskas@hotmail.com>
2022-06-10 11:47:18 -0300

    atualizando final

--- Exercicio-PHP-com-POO/index.php
+++ Exercicio-PHP-com-POO/index.php
@@ -7,7 +7,7 @@
     <title>Exemplo 3</title>
 
     <style>
-        table, th, td {
+    table, th, td {
         border: 2px solid black;
         border-collapse: collapse;
     }
@@ -16,7 +16,6 @@
         margin: auto;
         max-width: 700px;
     }
-
     th {
         background-color: black;
         color: white;
@@ -28,12 +27,10 @@
         width: 33%;
         padding: 0.5rem;
     }
-
     td:first-child {
         text-transform: uppercase;
     }
-
-    td:nth-child(3) {
+    td:nth-child(3n) {
         text-align: center;
     }
     </style>
@@ -51,21 +48,36 @@
 
 <?php
     // Importando a Classe
-    require_once "src/Livro.php";
+    require_once "src/Programacao.php";
+    require_once "src/Didatico.php";
 
     // Criação dos objetos
-    $livroA = new Livro;
-    $livroB = new Livro;
+    $livroA = new Programacao;
+    $livroB = new Didatico;
+    
     
     // Atribuindo dados via setters do objeto
-    $livroA->setTitulo("Ninguém é de Ninguém");
-    $livroA->setAutor("Zíbia Gasparetto");
-    $livroA->setPaginas("7845");
+    
 
-    $livroB->setTitulo("Perdidos na Noite");
-    $livroB->setAutor("Aloprado");
-    $livroB->setPaginas("239");
+    // Livro
+    $livroA->setTitulo("Perdidos na Noite");
+    $livroA->setAutor("Aloprado");
+    $livroA->setPaginas("239");
+    // Tecnico
+    $livroA->setFormato(['digital']);
+    // Programacao
+    $livroA->setArea("web");
+     
 
+    // Livro
+    $livroB->setTitulo("Coisas de Maluco para Aprender");
+    $livroB->setAutor("Shing-Ling");
+    $livroB->setPaginas("9632");
+    // Tecnico
+    $livroB->setFormato(["fisico"]);
+    // Didatico
+     $livroB->setDisciplina("inglês");
+     $livroB->setNivel(["basico"]);
 
 ?>
 
@@ -73,8 +85,19 @@
 <!-- <h2>Dados dos objetos (leitura via getters)</h2>
 <h3>Título: <?= $livroA->getTitulo() ?></h3>
 <p><strong>Autor: </strong><?= $livroA->getAutor() ?></p>
-<p><strong>Páginas: </strong> <?= $livroA->getPaginas() ?></p> -->
+<p><strong>Páginas: </strong> <?= $livroA->getPaginas() ?></p>
+<p><strong>Formato: </strong> <?= $livroA->getFormato() ?></p>
+<p><strong>Area: </strong> <?= $livroA->getArea() ?></p>
 <hr>
+
+<h3>Título: <?= $livroB->getTitulo() ?></h3>
+<p><strong>Autor: </strong><?= $livroB->getAutor() ?></p>
+<p><strong>Páginas: </strong> <?= $livroB->getPaginas() ?></p>
+<p><strong>Formato: </strong> <?= $livroB->getFormato() ?></p>
+<p><strong>Disciplina: </strong> <?= $livroB->getDisciplina() ?></p>
+<p><strong>Nível: </strong> <?= $livroB->getNivel() ?></p>
+<hr> -->
+
 
 <table>
     <thead>
@@ -82,6 +105,9 @@
             <th>Título</th>
             <th>Autor</th>
             <th>Páginas</th>
+            <th>Formato</th>
+            <th>Área/Disciplina</th>
+            <th>Nível</th>
         </tr>
     </thead>
     <tbody>
@@ -89,11 +115,17 @@
             <td><?= $livroA->getTitulo() ?></td>
             <td><?= $livroA->getAutor() ?></td>
             <td><?= $livroA->getPaginas() ?></td>
+            <td><?= $livroA->getFormato() ?></td>
+            <td>A-<?= $livroA->getArea() ?></td>
+            <td>#</td>
         </tr>
         <tr>
             <td><?= $livroB->getTitulo() ?></td>
             <td><?= $livroB->getAutor() ?></td>
             <td><?= $livroB->getPaginas() ?></td>
+            <td><?= $livroB->getFormato() ?></td>
+            <td>D-<?= $livroB->getDisciplina() ?></td>
+            <td><?= $livroB->getNivel() ?></td>
         </tr>
     </tbody>
 
---
+++ Exercicio-PHP-com-POO/src/Didatico.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "Tecnico.php";
+class Didatico extends Tecnico {
+    private string $disciplina;
+    private array $nivel = ['basico', 'medio', 'superior'];
+
+    // disciplina
+    public function getDisciplina(): string
+    {
+        return $this->disciplina;
+    }
+
+    public function setDisciplina(string $disciplina):void {
+        $this->disciplina = $disciplina;
+    }
+
+    // nivel
+    public function getNivel(): string {
+        return implode($this->nivel);
+    }
+
+   
+    public function setNivel(array $nivel):void {
+        $this->nivel = $nivel;
+    }
+}
