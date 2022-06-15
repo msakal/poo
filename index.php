a01c33d09da977ecbcb14220dd2f0f40bbc2b2ca
msakal <m_sakalauskas@hotmail.com>
2022-06-15 11:55:31 -0300

    finalizando exemplo7

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
     <title>Exemplo 5</title>
 </head>
 <body>
-    <h1>PHP POO - Exemplo 6</h1>
+    <h1>PHP POO - Exemplo 7</h1>
     <hr>
     <h2>Assuntos aborados:</h2>
     <ul>
-        <li>Polimorfismo</li>
+        <li>Propriedades e Métodos Estáticos</li>
     </ul>
     <hr><hr>
 
@@ -20,74 +20,36 @@
     // Importando a Classe
     // require_once "src/Cliente.php";  // Classe acesada pelas subClasses (PF / PJ)
     require_once "src/PessoaFisica.php";
-    require_once "src/PessoaJuridica.php";
+    // require_once "src/PessoaJuridica.php";
     // require_once "src/MEI.php";  // subClasse
 
     $clientePF = new PessoaFisica;
-    $clientePJ = new PessoaJuridica;
-    // $clienteMEI = new MEI;
-
     // Consulta PF
     $clientePF->setNome('Marcello');
-    $clientePF->setEmail('msa@hotmail.com');
-    $clientePF->setSenha('1234abc');
-
-    $clientePF->setCpf('123.456.789-00');
     $clientePF->setIdade(48);
 
+    
+    
+    // Acessando uma propriedade estática
+    PessoaFisica::$cidade = "São Paulo";
 
-    // Consulta PJ
-    $clientePJ->setNome('MarcelloSA');
-    $clientePJ->setEmail('msapj@hotmail.com');
-    $clientePJ->setSenha('78945xyz');
-
-    $clientePJ->setCnpj('60.662.996/0001-22');
-    $clientePJ->setAnoFundacao(1973);
-    // $clientePJ->setNomeFantasia('CursoTI');
-    $clientePJ->setNome('CursoTI');
-
-    // --------------------------------------------------------------------------------------------
-    // OBS: Para o polimorfismo ser possível, é obrigatório que os métodos tenham a mesma assinatura.
-    // 
-    // O que é assinatura de método?
-    // -Mesmo nome
-    // -Mesma quantidade de parâmetro
-    // -Mesmo tipo de dados nos parâmetros
-    // -Mesmo tipo de retorno (void, self, string ..)
-    // --------------------------------------------------------------------------------------------
-
-    // MEI
-    // $clienteMEI->setNome('Sakal');
-    // $clienteMEI->setNomeFantasia('Alemão');
-    // $clienteMEI->setCnpj('45.678.987/0001-99');
-    // $clienteMEI->setAreaDeAtuacao('Musica');
-
+    // Acessando e lendo o valor de uma propriedade estática
+    echo PessoaFisica::$cidade;
     
 ?>
+
+
+
 <p><strong>Pessoa Física</strong></p>
-<pre> <?=var_dump($clientePF)?> </pre>
 <p><?=$clientePF->getNome()?></p>
-<hr>
-<p><strong>Pessoa Jurídica</strong></p>
-<pre> <?=var_dump($clientePJ)?> </pre>
-<p><?=$clientePJ->getNome()?></p>
-<hr>
-
-<p><strong>Chamada Pela subClasse (PF)</strong></p>
-<section><?=$clientePF->exibirDados()?></section>
+<p><?=$clientePF->getIdade()?></p>
+<p>Tipo de Atendimento:
+    <?=PessoaFisica::verificaIdade( $clientePF->getIdade() ) ?>
+</p>
 <hr>
 
-<p><strong>Chamada Pela subClasse (PJ)</strong></p>
-<section><?=$clientePJ->exibirDados()?></section>
-
-<!-- <p><strong>MEI</strong></p>
-<pre> <?=var_dump($clienteMEI)?> </pre>
-<hr> -->
 
 
-<!-- ERRO ... pois 'Cliente' é abstrato -->
-<!-- <?php $cliente = new Cliente; ?>
-<pre><?=var_dump($cliente)?></pre> -->
 
 
 </body>
